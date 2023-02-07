477a62d6eaaed31bca73b7d369d8db19c7983b6a
alavazjordan <124105392+alavazjordan@users.noreply.github.com>
2023-02-06 20:05:46 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,29 @@
+package main;
+
+import java.awt.Container;
+
+import javax.swing.JFrame;
+
+public class Main extends JFrame{
+ 
+
+    public Main(){
+        Container pane = getContentPane();
+        MyPanel mp = new MyPanel();
+        pane.add(mp);
+
+        pack();
+        setVisible(true);
+        
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(false);//doesn't allow for window to be resized
+        setLocationRelativeTo(null);// sets window in the center of screen
+
+
+        mp.startGameThread();
+    }
+
+    public static void main(String[] args){
+        new Main();
+    }
+}
